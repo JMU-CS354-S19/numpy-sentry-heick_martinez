1adc544a9f604c1155face821ac0b637befcf3d7
Heick <heickrd@l200208.cs.jmu.edu>
2019-01-24 13:46:19 -0500

    message

--- scripts/sentry.py
+++ scripts/sentry.py
@@ -11,6 +11,7 @@ import rospy
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from kobuki_msgs.msg import Sound
 import numpy as np
 
 
@@ -32,14 +33,34 @@ class SentryNode(object):
         self.cv_bridge = CvBridge()
         rospy.Subscriber('/camera/depth_registered/image',
                          Image, self.depth_callback, queue_size=1)
+        
+        rospy.Publisher('/mobile_base/commands/sound', Sound) \
+        self.sound = Sound()
+        self.p = np.array()
+        self.first = true
+        self.average = 1
         rospy.spin()
 
     def depth_callback(self, depth_msg):
         """ Handle depth callbacks. """
-
+        
         # Convert the depth message to a numpy array
         depth = self.cv_bridge.imgmsg_to_cv2(depth_msg)
-
+        if first:
+            p = depth[:, 240]
+            
+        else:
+            c = depth[:, 240]
+            norm = np.absolute(np.subtract(c - p))
+            norm = np.sum(norm)
+            average = average * .5 + norm * (1 - .5)
+            
+            if (norm/average) > 1.5:
+                self.sound.value = 0
+            else:
+                self.sound.value = 1
+            rospy.publish(self.sound)
+            
         # YOUR CODE HERE.
         # HELPER METHODS ARE GOOD.
 
